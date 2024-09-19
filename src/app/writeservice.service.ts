4236b0fb5e4f92696da3ef052f4edf061b8eac4c
Ronak-Ronu <ronakronu02@gmail.com>
2024-09-19 19:29:51 +0530

    update like feature

--- src/app/writeservice.service.ts
+++ src/app/writeservice.service.ts
@@ -12,11 +12,11 @@ export class WriteserviceService {
   
   constructor(private http:HttpClient) {
 
-    // this.url=`${environment.beronyAPI}/api/posts`
-    // this.drafturl=`${environment.beronyAPI}/api/drafts`
+    this.url=`${environment.beronyAPI}/api/posts`
+    this.drafturl=`${environment.beronyAPI}/api/drafts`
     
-    this.url='http://localhost:3000/api/posts'
-    this.drafturl='http://localhost:3000/api/drafts'
+    // this.url='http://localhost:3000/api/posts'
+    // this.drafturl='http://localhost:3000/api/drafts'
    }
 
       
